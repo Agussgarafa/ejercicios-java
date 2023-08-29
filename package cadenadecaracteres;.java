4ec4d6d8436352dea5cfeecc5b538f2edb80e379
Agussgarafa <142992173+Agussgarafa@users.noreply.github.com>
2023-08-29 16:22:09 -0300

    Add files via upload

---
+++ package cadenadecaracteres;.java
@@ -0,0 +1,16 @@
+package cadenadecaracteres;
+import java.util.Scanner; 
+public class ej1 {
+
+	public static void main(String[] args) {
+		var scanner = new Scanner(System.in);
+		System.out.println("ingrese su nombre: ");
+		var nombre = scanner.nextLine();
+		
+		
+		System.out.println("su nombre es:  "+ nombre);
+	}
+
+}
+
+
